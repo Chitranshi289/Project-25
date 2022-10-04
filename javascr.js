ce31ec228e3d12454ea501985ddac1782932748d
chitranshi289 <chitranshi289@gmail.com>
2022-10-04 16:43:27 +0530

    message

---
+++ javascr.js
@@ -0,0 +1,86 @@
+//JAVASCRIPT ARTHIMETIC OPERATORS :
+let  a=40;
+let b=20;
+let c= a+b;
+console.log("+ operator")
+console.log(c);
+let  a1=40;
+let b1=20;
+let c1= a*b;
+console.log("* operator")
+console.log(c1);
+let  a2=40;
+let b2=20;
+let c2= a-b;
+console.log("- operator")
+console.log(c2);
+let  a3=40;
+let b3=20;
+let c3= a**b;
+console.log("**operator")
+console.log(c3);
+let  a4=40;
+let b4=20;
+console.log("% operator")
+let c4= a%b;
+console.log(c4);
+let  a5=40;
+let b5=20;
+let c5= a/b;
+console.log("/ operator")
+console.log(c5);
+// JAVASCRIPT RELATIONAL OPERATOR:
+console.log(" == operator")
+console.log(5==9);
+console.log("> operator")
+console.log(5>3)
+console.log(">= operator")
+console.log(6>=6)
+console.log("<= operator")
+console.log(6<=9)
+console.log("1== operator")
+console.log(3!==5);
+console.log("!= operator")
+console.log(4!=8)
+// JAVASCRIPT ASSIGNMENT OPERATOR:
+let d =6;
+console.log("= operator")
+console.log(d);
+
+console.log("+= operator")
+let x = 10;
+x += 5;
+console.log(x)
+console.log("-= operator")
+let x1 = 10;
+x1 -= 5;
+console.log(x1)
+console.log("*= operator")
+let x2 = 10;
+x2 *= 5;
+console.log(x2)
+console.log("/= operator")
+let x3 = 10;
+x3 /= 5;
+console.log(x3)
+console.log("**= operator")
+let x4= 10;
+x4 **= 5;
+console.log(x4)
+// JAVASCRIPT BITWISE OPERATOR
+console.log(" & operator")
+console.log(5&1)
+console.log("| operator")
+console.log (5 | 1)
+console.log("~ operator")
+console.log(~5)
+console.log("^ operator")
+console.log(5^1)
+console.log("<< operator")
+console.log(4<<5)
+console.log(">> operator")
+console.log(4>>5)
+console.log(">>> operator")
+console.log(4>>>6)
+
+
